436ad5e7734e2dde47d33d56d7dc2860af5d41f2
Bruno Bonacci <bruno.bonacci@gmail.com>
2023-08-21 11:01:48 +0100

    Bumped dependencies and released 0.3.1

--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
 
   :scm {:name "git" :url "https://github.com/BrunoBonacci/ip-geoloc"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.maxmind.geoip2/geoip2 "2.13.1"]
-                 [clj-http "3.10.0"]
-                 [pandect "0.6.1"]
-                 [com.brunobonacci/safely "0.5.0"]
-                 [org.clojure/tools.logging "1.0.0"]])
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.maxmind.geoip2/geoip2 "4.1.0"]
+                 [clj-http "3.12.3"]
+                 [pandect "1.0.2"]
+                 [com.brunobonacci/safely "0.7.0-alpha3"]
+                 [org.clojure/tools.logging "1.2.4"]])
